71bd810754720a55e5caadf56af8100a54c4cad8
Michelangelo Lv <mgaligigi@163.com>
2023-06-16 17:11:39 +0800

    feat:Lingo3D Demo

---
+++ src/components/Lingo3D.vue/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { World, Model,OrbitCamera } from "lingo3d-vue"
+</script>
+
+<template>
+  <World>
+    <Model
+      src="../../../public/AnimeGirl.fbx"
+      :scale="5"
+      toon
+     
+    ></Model>
+    <!-- <OrbitCamera active enable-zoom ></OrbitCamera> -->
+  </World>
+</template>
--- src/views/SweetHome/Apps/AnimeGirl/index.vue
+++ src/views/SweetHome/Apps/AnimeGirl/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    <Live2d></Live2d>
+    <!-- <Live2d></Live2d> -->
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue"
-import Live2d from '@/components/Live2d/index.vue'
+// import Live2d from '@/components/Live2d/index.vue'
+
 </script>
 
 <style lang="less" scoped>
